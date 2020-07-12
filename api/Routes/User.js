cfd97645e15eaeae35a1dcbe72c9d01f1bf1e5f8
habib <has.sorathia@gmail.com>
2020-07-11 21:56:28 -0700

    Updated routes to support decoupled frontend

--- api/Routes/User.js
+++ api/Routes/User.js
@@ -3,32 +3,38 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/UserDB.js')
+const User = require('../Models/UserDB.js')
+
+router.post("/", function(req, res, next) {
+    res.send("API is working properly");
+});
 
 router.post('/register', async (req, res) => {
-    console.log(req);
+    console.log(req.body);
     const userToRegister = req.body;
-    if (userToRegister.email && userToRegister.password && userToRegister.username) {
-        const newUser = new User({
-            username: userToRegister.username,
-            firstName: userToRegister.firstname,
-            lastName: userToRegister.lastName,
-            password: userToRegister.password,
-            email: userToRegister.email
-        });
-        console.log('nice we made it here');
-        // await newUser.save()
-        //   .catch(error => {
-        //       console.log(error);
-        //       res.status("CONFLICT").send({ message: error });
-        //   })
-        //   .then(_ => {
-        //       console.log('sucess');
-        //       res.status('SUCCESS').send({ message: 'Account creation successful' });
-        //   })
-    } else {
-        res.sendStatus({ message: 'Missing email, username, and/or password.' });
-    }
+    console.log('haha all your information is ours');
+    res.send({ message: 'haha thanks for your information xddddd' });
+    // if (userToRegister.email && userToRegister.password && userToRegister.username) {
+    //     const newUser = new User({
+    //         username: userToRegister.username,
+    //         firstName: userToRegister.firstname,
+    //         lastName: userToRegister.lastName,
+    //         password: userToRegister.password,
+    //         email: userToRegister.email
+    //     });
+    //     console.log('nice we made it here');
+    //     // await newUser.save()
+    //     //   .catch(error => {
+    //     //       console.log(error);
+    //     //       res.status("CONFLICT").send({ message: error });
+    //     //   })
+    //     //   .then(_ => {
+    //     //       console.log('sucess');
+    //     //       res.status('SUCCESS').send({ message: 'Account creation successful' });
+    //     //   })
+    // } else {
+    //     res.sendStatus({ message: 'Missing email, username, and/or password.' });
+    // }
 });
 
 // router.post('/login', function (req, res) {
@@ -41,3 +47,5 @@ router.post('/register', async (req, res) => {
 // this file and others in this folder will be responsible for
 // handling api responses from our website
 // things like the usersigning up for something
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const server = express()
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+const index = require('./api/Routes/User');
 
 server.use(cors());
 server.use(express.json());
 
+server.use('/', index);
 server.listen(8080, () => {
     console.log('Server listening on port 8080');
 })
