5cc808cfff675a4f2eeb58fd798a933489ebf16f
Jerry Lee <jerrylee1157@gmail.com>
2020-10-05 01:54:49 -0700

    Modified package

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routing from './Routing';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
