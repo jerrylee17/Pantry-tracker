567c87213128e2f3807f52d8786fdd1fa33480c7
Habib <has.sorathia@gmail.com>
2020-10-12 21:16:33 -0700

    hotfix server rm getUser query for current user

--- server/server.js
+++ server/server.js
@@ -11,26 +11,26 @@ const server = new ApolloServer({
     schema,
     playground: true,
     introspection: true,
-    context: ({ req }) => {
-        // Note! This example uses the `req` object to access headers,
-        // but the arguments received by `context` vary by integration.
-        // This means they will vary for Express, Koa, Lambda, etc.!
-        //
-        // To find out the correct arguments for a specific integration,
-        // see the `context` option in the API reference for `apollo-server`:
-        // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
-     
-        // Get the user token from the headers.
-        const token = req.headers.authorization || '';
-     
-        // try to retrieve a user with the token
-        const user = getUser(token);
-     
-        // add the user to the context
-        return { user };
-    },
-});
 
+});
+// context: ({ req }) => {
+//     // Note! This example uses the `req` object to access headers,
+//     // but the arguments received by `context` vary by integration.
+//     // This means they will vary for Express, Koa, Lambda, etc.!
+//     //
+//     // To find out the correct arguments for a specific integration,
+//     // see the `context` option in the API reference for `apollo-server`:
+//     // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+ 
+//     // Get the user token from the headers.
+//     const token = req.headers.authorization || '';
+ 
+//     // try to retrieve a user with the token
+//     const user = getUser(token);
+ 
+//     // add the user to the context
+//     return { user };
+// },
 const app = express();
 app.use(cors());
 server.applyMiddleware({ app });
