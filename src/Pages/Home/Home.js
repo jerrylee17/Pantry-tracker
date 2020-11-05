106ae8cba93f06351e70af41e70cae6562fffb8e
Habib <has.sorathia@gmail.com>
2020-11-05 13:52:16 -0800

    finished base css for homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,8 @@ import {
   Paper,
   Grid,
   Typography,
-  Button
+  Button,
+  Hidden
 } from '@material-ui/core';
 import food_mid from './images/food_mid.svg';
 import map from './images/map.svg';
@@ -42,29 +43,146 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: theme.typography.fontFamily
   },
   mainimage: {
-    width: '100%',
-    height: '100%'
+    paddingLeft: "20%",
+    minHeight: '40vh',
+    minWidth: '30vw',
+    width: '70%',
+    height: '80%',
+  },
+  boxesRow: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 'auto',
   },
   boxes: {
     backgroundColor: '#D3D7FF',
-    margin: '2vh 3vw 3vh 3vw',
+    margin: '4vh 4vw 4vh 4vw',
     justifyContent: 'center',
-    height: 'auto',
-    minHeight: '20vh',
+    alignItems: 'center',
+    maxHeight: '195px',
+    maxWidth: '288px',
+    minHeight: '195px',
     flex: '50%',
     flexDirection: 'column',
-    fontFamily: theme.typography.fontFamily
+    fontFamily: theme.typography.fontFamily,
+    padding: theme.spacing(1),
   },
-  miniimage: {
+  miniImagePepTalk : {
     paddingTop: '5%',
-    paddingLeft: '25%',
-    paddingRight: '25%',
-    width: '50%',
-    height: '50%',
+    paddingLeft: '23%',
+  },
+  leftImage: {
+    paddingLeft: '28%',
+    width: '45%',
+    height: '45%',
+  },
+  midImage: {
+    paddingTop: '8%',
+    width: '60%',
+    height: '60%',
+  },
+  rightImage: {
+    paddingLeft: '30%',
+    paddingTop: "4%",
+    width: '47%',
+    height: '47%',
+  },
+  textbox: {
+    margin: '0',
+    height: '150px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  mainText: {
+    paddingRight: "25%"
   },
   typography: {
     padding: '4px',
     textAlign: 'center',
+  },
+  textRow: {
+    marginTop: '0px',
+    marginBottom: '5px',
+    padding: '0px',
+  },
+  subtitleText: {
+    ...theme.typography.fontWeightLight,
+    ...theme.typography.overline,
+  },
+  headerText: {
+    marginTop: '0',
+    ...theme.typography.fontWeightBold,
+    ...theme.typography.h5,
+  },
+  definitionText: {
+    ...theme.typography.fontWeightBold,
+    ...theme.typography.h7,
+    width: '35em',
+  },
+  mapRow: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(5),
+    margin: 'auto',
+  },
+  mappedImgBox: {
+    paddingTop: '0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 'auto',
+  },
+  break: {
+    flexBasis: '100%',
+    height: '20vh',
+  },
+  textMap: {
+    width: '35em',
+  },
+  mapHiddenBox: {
+    [theme.breakpoints.down('sm')]: {
+      maxHeight: '0px',
+      maxWidth: '0px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      padding: '4px',
+      textAlign: 'center',
+    }
+  },
+  map: {
+    width: '90vw',
+  },
+  learnAboutUsPaper: {
+    backgroundColor: '#D3D7FF',
+    margin: '4vh 4vw 4vh 4vw',
+    justifyContent: 'center',
+    // alignItems: 'center',
+    // maxHeight: '100px',
+    minHeight: '120px',
+    maxWidth: '72px',
+    minWidth: '50px',
+    flex: '50%',
+    // flexDirection: 'column',
+    fontFamily: theme.typography.fontFamily,
+    padding: theme.spacing(1),
+  },
+  learnAboutUsBanner: {
+    margin: '0px',
+    // height: '400px',
+    marginBottom: '10px'
+  },
+  aboutUsText: {
+    padding: '4px',
+    textAlign: 'left',
+  },
+  disclaimerText: {
+    margin: '0px',
+    justifyContent: 'center',
+
   },
   introfont: {
     fontSize: '150%',
@@ -100,7 +218,9 @@ export default function Home(props) {
               className={classes.paper}
               elevation={0}
             >
-              <Typography>
+              <Typography
+                className={classes.mainText}
+              >
                 <h1>A Pantry management system that just works</h1>
                 <body1 className={classes.introfont}>Figure out the food items in your pantry with a click of a button.</body1>
               </Typography>
@@ -108,108 +228,161 @@ export default function Home(props) {
           </Grid>
         </Grid>
         <Grid
-          className={classes.darkbanner}
+          className={`${classes.boxesRow} ${classes.darkbanner}`}
           container
-          justify='center'
-          spacing={3}
+          spacing={1}
           xs={12}
         >
-          <Grid item xs={4}>
-            <Paper
-              className={classes.boxes}
-              justifyContent='center'
-              elevation={3}
-            >
-              <img
-                className={classes.miniimage}
-
-                src={speed_left}
-                alt="fast updates"
-              />
-              <Typography
-                className={classes.typography}
-              >
-                Changes in your pantry will be reflected on the website immediately
-              </Typography>
-            </Paper>
-
-          </Grid>
-          <Grid item xs={4}>
-            <Paper
-              className={classes.boxes}
-              elevation={3}
-            >
-              <img
-                className={classes.miniimage}
-                src={food_mid}
-                alt="food suggestions"
-              />
-              <Typography
-                className={classes.typography}
-              >
-                Changes in your pantry will be reflected on the website immediately
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper
-              className={classes.boxes}
-              elevation={3}
-            >
-              <img
-                className={classes.miniimage}
-                src={stats_right}
-                alt="can see pantry status"
-              />
-              <Typography
-                className={classes.typography}
-              >
-              Changes in your pantry will be reflected on the website immediately
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-        <Grid
-          className={classes.lightbackground}
-          container
-          justify='center'
-          spacing={3}
-          xs={12}
-        >
-          <Typography>
-            MANAGEMENT AND MONITORING TOOLS FOR YOUR Pantry
-            Tracking the items in your pantry has never been easier
-            Our Pantry tracking solution empowers users with one to several pantries to manage and view their pantries in real time.
-          </Typography>
-        </Grid>
-        <Grid
-          container
-          className={classes.darkbanner}
-          justify='center'
-          spacing={3}
-          xs={12}
-        >
-          <Grid
-            item
-            xs={12}
+          <div
+            className={classes.boxesRow}
           >
-            <Typography>
+            <Grid 
+              item 
+              xs={4}
+              justifyContent='center'
+              >
+              <Paper
+                className={classes.boxes}
+                justifyContent='center'
+                elevation={3}
+                spacing={3}
+              >
+                <img   
+                  className={`${classes.miniImagePepTalk} ${classes.leftImage}`}
+                  src={speed_left}
+                  alt="fast updates"
+                />
+                <Typography
+                  className={classes.typography}
+                >
+                  Changes in your pantry will be reflected on the website immediately
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid 
+              item 
+              xs={4}
+            >
+              <Paper
+                className={classes.boxes}
+                elevation={3}
+              >
+                <img
+                  className={`${classes.miniImagePepTalk} ${classes.midImage}`}
+                  src={food_mid}
+                  alt="food suggestions"
+                />
+                <Typography
+                  className={classes.typography}
+                >
+                  Get smart food suggestions based on what you eat
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid 
+              item 
+              xs={4}
+            >
+              <Paper
+                className={classes.boxes}
+                elevation={3}
+              >
+                <img
+                  className={`${classes.miniImagePepTalk} ${classes.rightImage}`}
+                  src={stats_right}
+                  alt="can see pantry status"
+                />
+                <Typography
+                  className={classes.typography}
+                >
+                  Track information for as many pantries as you want
+                </Typography>
+              </Paper>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid
+          container
+          className={`${classes.lightbackground} ${classes.textbox}`}
+          justify='center'
+          spacing={3}
+          xs={12}
+        >
+            <Typography
+              className={`${classes.typography} ${classes.subtitleText}`}
+              inline
+            >
+              <em>MANAGEMENT AND MONITORING TOOLS FOR YOUR Pantry</em>
+            </Typography>
+            <Typography
+              className={`${classes.typography} ${classes.headerText}`}
+              inline
+            >
+              Tracking the items in your pantry has never been easier
+            </Typography>
+            <Typography
+              className={`${classes.typography} ${classes.definitionText}`}
+              inline
+            >
+              Our Pantry tracking solution empowers users with one to several pantries to manage and view their pantries in real time.
+            </Typography>
+        </Grid>
+        <Grid
+          container
+          className={`${classes.darkbanner} ${classes.mapRow}`}
+          justify='center'
+          spacing={3}
+          xs={12}
+        >
+          <div
+            className={classes.mappedImgBox}
+          >
+            <Typography
+              className={`${classes.typography} ${classes.headerText} ${classes.textMap}`}
+              inline
+            >
               How it comes together
             </Typography>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <img
-              className={classes.mainimage}
-              src={map}
-              alt="map"
-            />
-          </Grid>
+            <div className={classes.break}/>
+            <Hidden smDown>
+              <div
+                className={`${classes.mapHiddenBox}`}
+              >
+                <img
+                  className={classes.map}
+                  src={map}
+                  alt="map"
+                />
+              </div>
+            </Hidden>
+            <Hidden smUp>
+              <div
+                className={classes.mapHiddenBox}
+              >
+                <Typography
+                  className={`${classes.typography} ${classes.subtitleText}`}
+                  inline
+                >
+                  <em>MANAGEMENT AND MONITORING TOOLS FOR YOUR Pantry</em>
+                </Typography>
+                <Typography
+                  className={`${classes.typography} ${classes.headerText}`}
+                  inline
+                >
+                  Tracking the items in your pantry has never been easier
+                </Typography>
+                <Typography
+                  className={`${classes.typography} ${classes.definitionText}`}
+                  inline
+                >
+                  Our Pantry tracking solution empowers users with one to several pantries to manage and view their pantries in real time.
+                </Typography>
+              </div>
+            </Hidden>
+          </div>
         </Grid>
         <Grid
-          classname={classes.darkbanner}
+          className={`${classes.learnAboutUsBanner} ${classes.boxesRow}`}
           container
           justify='center'
           spacing={3}
@@ -221,38 +394,54 @@ export default function Home(props) {
             spacing={3}
             xs={12}
           >
-            <Paper>
-              <Typography>
-                LEARN MORE ABOUT US
-                Interested in the application and it's creators?
-              </Typography>
-            </Paper>
+            <Typography
+              className={`${classes.typography} ${classes.subtitleText}`}
+              inline
+            >
+              LEARN MORE ABOUT US
+            </Typography>
+            <Typography
+              className={`${classes.typography} ${classes.headerText}`}
+              inline
+            >
+              Interested in the application or its creators?
+            </Typography>
           </Grid>
-          <Grid
-            item
-            justify='center'
-            xs={6}
+          <Paper
+            className={`${classes.lightbackground} ${classes.typography}`}
+            // elevation={1}
           >
-            <Paper>
-              <Typography>
-                What's in your pantry was created by seniors at San Jose State University
-              </Typography>
-              <Button>Learn More</Button>
-            </Paper>
-
-          </Grid>
-          <Grid
-            item
-            justify='center'
-            xs={6}
+            <Typography
+              className={classes.aboutUsText}
+            >
+              What's in your pantry was created by seniors at San Jose State University
+            </Typography>
+            <Button>Learn More</Button>
+          </Paper>
+          <Paper
+            className={`${classes.lightbackground} ${classes.typography}`}
+            // elevation={1}
           >
-            <Paper>
-              <Typography>
-                The application and all of the code needed to run it can be found on github
-              </Typography>
-              <Button>Learn More</Button>
-            </Paper>
-          </Grid>
+            <Typography
+              className={classes.aboutUsText}
+            >
+              The application and all of the code needed to run it can be found on github
+            </Typography>
+            <Button>Learn More</Button>
+          </Paper>
+        </Grid>
+        <Grid
+          container
+          className={`${classes.darkbackground} ${classes.darkbanner} ${classes.disclaimerText}`}
+          spacing={3}
+          xs={12}
+        >
+          <Typography
+            className={`${classes.typography} ${classes.subtitleText}`}
+            inline
+          >
+            What's in your pantry - Copyright © 2020
+          </Typography>
         </Grid>
       </div>
     </>
