033d806f3c285f71e277c281fbf82f30507b0af3
Habib <has.sorathia@gmail.com>
2020-11-20 23:10:41 -0800

    wip

--- server/src/__tests__/query.test.js
+++ server/src/__tests__/query.test.js
@@ -2,33 +2,57 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const { createTestClient } = require('apollo-server-testing');
 const { schema } = require('../schema/index.js');
 
-const mocks = {
-    _id: () => '5fffff',
-    String: () => 'hi',
+/*
+    Come up with better ways to mock data and then pass it into our server
+    we then will query our server with that data and then check to see if values
+    match up which is how we'll verify that it went through.
+*/
+
+const mockPantryResponse = {
+    _id: '5fffff',
+    name: 'hi',
+    contents: [
+        'rice',
+        'crackers',
+        'am die',
+    ]
+}
+
+const mockPantry = {
+    pantry: () => ({
+        _id: () => '5233',
+        name: () => 'hi',
+        contents: () => [
+            'rice',
+            'crackers',
+        ]
+    })
+}
+let pantryAPI = jest.fn(() => [mockPantryResponse]);
+    
+const otherMock = {
+    _id: () => '5aaaaaaa',
+    String: () => 'bye',
 }
 
 const server = new ApolloServer({
     schema,
-    mocks
+    dataSources: () => (mockPantry)
 });
 
 const { query, mutate } = createTestClient(server);
 
 test('does a simple pantry many query', async () => {
-    const PANTRYMANY = gql`
-        query {
-            pantryMany {
-                name
-            }
-        }
-    `;
-    
-    const {
-        data: { pantryMany }
-    } = await query({ query: PANTRYMANY })
+    // const PANTRYMANY = gql`
+    //     pantryMany {
+    //         name
+    //     }
+    // `;
+
+    const data = await query({ query: '{ pantryOne(filter: {name:"hi"}) { name } }' })
 
     // const res = await query({ query: QUERY });
-    console.log(pantryMany);
+    console.log(data);
     expect(2).toBe(2);
 
 });
