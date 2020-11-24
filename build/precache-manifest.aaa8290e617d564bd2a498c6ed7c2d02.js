f29a04bedf6ef3ea1e11d3b227dcde3a576f0bc9
Jerry Lee <jerrylee1157@gmail.com>
2020-11-23 20:53:11 -0800

    Removed build/ from gitignore
    Reason: The EC2 instance did not have enough memory to run `npm run build`.

---
+++ build/precache-manifest.aaa8290e617d564bd2a498c6ed7c2d02.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d43b5708da9dbcadc8a7d449c76632a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40bf1bd1bc6bec545540",
+    "url": "/static/css/main.c7c4bc05.chunk.css"
+  },
+  {
+    "revision": "64043ed9d1ebf70f71ce",
+    "url": "/static/js/2.5b37a5d1.chunk.js"
+  },
+  {
+    "revision": "7432e8b096e334eb22c784efef8eda5b",
+    "url": "/static/js/2.5b37a5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bf1bd1bc6bec545540",
+    "url": "/static/js/main.701cb52a.chunk.js"
+  },
+  {
+    "revision": "30b06da9db414241b205",
+    "url": "/static/js/runtime-main.1be33307.js"
+  },
+  {
+    "revision": "3bb0ae4e4bd0f0fe8222c2f0962669da",
+    "url": "/static/media/background.3bb0ae4e.jpg"
+  },
+  {
+    "revision": "f2cd2055896b5f8727399f6c49a11d8a",
+    "url": "/static/media/food_mid.f2cd2055.svg"
+  },
+  {
+    "revision": "76844eaf22cd38114f6e8b74226a4d14",
+    "url": "/static/media/harley.76844eaf.jpeg"
+  },
+  {
+    "revision": "6af9e6fbfc78662bbcd83ae2d2d0fde3",
+    "url": "/static/media/map.6af9e6fb.svg"
+  },
+  {
+    "revision": "5d45e56bbf51286e8d6349a0a1e4053e",
+    "url": "/static/media/pantrymanagement.5d45e56b.svg"
+  },
+  {
+    "revision": "442b56c02c67d7524958eea53800c907",
+    "url": "/static/media/speed_left.442b56c0.svg"
+  },
+  {
+    "revision": "c913e4a9a43e32efe604a9190bf136e9",
+    "url": "/static/media/stats_right.c913e4a9.svg"
+  }
+]);
