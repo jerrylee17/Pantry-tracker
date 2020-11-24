2f2043f72a7e9e50fe772227ee5a417d2fbb63fe
Jerry Lee <jerrylee1157@gmail.com>
2020-11-24 01:32:38 -0800

    Removed database connection

--- server/database.js
+++ server/database.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 
-require('dotenv').config();
-const url =
-  `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/` +
-  `${process.env.DB_NAME}`;
+// require('dotenv').config();
+// const url =
+//   `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/` +
+//   `${process.env.DB_NAME}`;
 
-mongoose.connect(url, {
-  autoIndex: true,
-  poolSize: 50,
-  bufferMaxEntries: 0,
-  keepAlive: 120,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(url, {
+//   autoIndex: true,
+//   poolSize: 50,
+//   bufferMaxEntries: 0,
+//   keepAlive: 120,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-mongoose.set('useCreateIndex', true);
+// mongoose.set('useCreateIndex', true);
 
-mongoose.connection.once('open', () =>
-  // eslint-disable-next-line
-  console.log(`Mongo connection established at ${url}`)
-);
+// mongoose.connection.once('open', () =>
+//   // eslint-disable-next-line
+//   console.log(`Mongo connection established at ${url}`)
+// );
